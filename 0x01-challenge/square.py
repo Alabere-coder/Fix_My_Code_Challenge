f8cb500e881c0605ab18b5eff5302effd5b6b459
Alabere-coder <alabere4real247@gmail.com>
2023-04-11 10:56:23 +0100

    updated

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -22,6 +22,7 @@ class Square():
         """String representation."""
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
